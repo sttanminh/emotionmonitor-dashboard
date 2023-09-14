cf7921990d9c766a98c9500f9efd16917332622d
Tuan Huynh <thuynh2001.dw@gmail.com>
2023-09-15 03:03:10 +1000

    added components for manage metrics section

---
+++ components/metricsSetting/metricsSetting.tsx
@@ -0,0 +1,40 @@
+import React from "react"
+import styles from "./metricsSetting.module.css"; // Import the CSS module
+import { FaEdit, FaRegTrashAlt } from 'react-icons/fa';
+import Link from 'next/link';
+
+interface MetricsSettingProps {
+}
+
+const MetricsSetting: React.FC<MetricsSettingProps> = () => {
+    return (
+        <div className={styles.metricContainer}>Complexity
+            <Link href="/">
+                <FaEdit size={18} style={{ color: "#50C878", marginLeft: "20px" }} />
+                <FaRegTrashAlt size={18} style={{ color: "#EE4B2B", marginLeft: "15px" }} />
+            </Link>
+            <div className={styles.levelContainer}><b>Levels:</b>
+                <div>Low
+                    <Link href="/">
+                        <FaEdit size={18} style={{ color: "#50C878", marginLeft: "20px" }} />
+                        <FaRegTrashAlt size={18} style={{ color: "#EE4B2B", marginLeft: "15px" }} />
+                    </Link>
+                </div>
+                <div>Medium
+                    <Link href="/">
+                        <FaEdit size={18} style={{ color: "#50C878", marginLeft: "20px" }} />
+                        <FaRegTrashAlt size={18} style={{ color: "#EE4B2B", marginLeft: "15px" }} />
+                    </Link>
+                </div>
+                <div>High
+                    <Link href="/">
+                        <FaEdit size={18} style={{ color: "#50C878", marginLeft: "20px" }} />
+                        <FaRegTrashAlt size={18} style={{ color: "#EE4B2B", marginLeft: "15px" }} />
+                    </Link>
+                </div>
+            </div>
+        </div >
+    );
+}
+
+export default MetricsSetting;
--- pages/config.tsx
+++ pages/config.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import MetricsSetting from "../components/metricsSetting/metricsSetting"
 
 const ConfigurationPage = () => {
 
-  return (
-    <div className="body-config">
-      <h1>Emotimonitor Configuration</h1>
-    </div>
-  );
+    return (
+        <div className="body-config">
+            <h1>Emotimonitor Configuration</h1>
+            <p>This page allow you to manage the content shown in the Trello PowerUp, including modifying, adding, or removing metrics and adjusting emoji for each level.</p>
+            <h2>Manage Metrics</h2>
+            <MetricsSetting/>
+        </div>
+    );
+
 };
 
 export default ConfigurationPage;
