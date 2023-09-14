fc2a258a7d0205f4afb9eaba706359b571ebc69b
Tuan Huynh <thuynh2001.dw@gmail.com>
2023-09-14 23:48:57 +1000

    initialise configuration page

---
+++ pages/config.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ConfigurationPage = () => {
+
+  return (
+    <div className="body-config">
+      <h1>Emotimonitor Configuration</h1>
+    </div>
+  );
+};
+
+export default ConfigurationPage;
