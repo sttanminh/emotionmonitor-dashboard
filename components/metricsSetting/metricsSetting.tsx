46a2e66515a8fd3777adce2e80b4533c3d39dc2c
Tuan Huynh <thuynh2001.dw@gmail.com>
2023-09-15 06:17:05 +1000

    UI overhaul

--- components/metricsSetting/metricsSetting.tsx
+++ components/metricsSetting/metricsSetting.tsx
@@ -9,10 +9,11 @@ interface MetricLevel {
     levelOrder: number
 }
 
-// represent the metric objet to be passed to the component. Consist of a name and a levels array (based on the data structure)
+// represent the metric objet to be passed to the component. Consist of a name, a levels array and the metric id (based on the data structure)
 interface Metric {
     metricName: string
     levels: MetricLevel[]
+    metricId: string
 }
 
 // determine what will be passed to the component. In this case, a metric object
@@ -22,16 +23,17 @@ interface MetricsSettingProps {
 
 const MetricsSetting: React.FC<MetricsSettingProps> = ({ metric }) => {
     return (
-        <div className={styles.metricContainer}>{metric.metricName}
+        <div className={styles.metricContainer}><b>{metric.metricName}</b>
             <Link href="/">
                 <FaEdit size={18} style={{ color: "#50C878", marginLeft: "20px" }} />
                 <FaRegTrashAlt size={18} style={{ color: "#EE4B2B", marginLeft: "15px" }} />
             </Link>
-            <div className={styles.levelContainer}><b>Levels:</b>
+
+            <div className={styles.levelsContainer}>
                 {
                     // for each element in the levels array we add a level div using map()
                     metric.levels.map((level) => ( // level is the current element of the metric.levels array
-                        <div>{level.levelLabel}
+                        <div className={styles.level}>{level.levelLabel}
                             <Link href="/">
                                 <FaEdit size={18} style={{ color: "#50C878", marginLeft: "20px" }} />
                                 <FaRegTrashAlt size={18} style={{ color: "#EE4B2B", marginLeft: "15px" }} />
--- pages/config.tsx
+++ pages/config.tsx
@@ -50,6 +50,25 @@ const ConfigurationPage = () => {
                     }, {
                         levelLabel: "High",
                         levelOrder: 3
+                    }, {
+                        levelLabel: "Extreme",
+                        levelOrder: 3
+                    }
+                ],
+                metricId: ''
+            },
+            {
+                metricName: 'Timespan',
+                levels: [
+                    {
+                        levelLabel: "Short",
+                        levelOrder: 1
+                    }, {
+                        levelLabel: "Reasonable",
+                        levelOrder: 2
+                    }, {
+                        levelLabel: "Long",
+                        levelOrder: 3
                     }
                 ],
                 metricId: ''
@@ -62,13 +81,15 @@ const ConfigurationPage = () => {
             <h1>Emotimonitor Configuration</h1>
             <p>This page allow you to manage the content shown in the Trello PowerUp, including modifying, adding, or removing metrics and adjusting emoji for each level.</p>
             <h2>Manage Metrics</h2>
-            {
-                // for each element in the metrics array we add a MetricsSetting component using map()
-                projectData.metrics.map((metric, index) => (
-                    // metric is the current element of the projectData.metrics array, index is the index of the current element in the projectData.metrics array
-                    <MetricsSetting metric={metric} />
-                ))
-            }
+            <div className='metric-container'>
+                {
+                    // for each element in the metrics array we add a MetricsSetting component using map()
+                    projectData.metrics.map((metric, index) => (
+                        // metric is the current element of the projectData.metrics array, index is the index of the current element in the projectData.metrics array
+                        <MetricsSetting metric={metric} />
+                    ))
+                }
+            </div>
         </div>
     );
 
