dec06937dd427b1d43e9da171cceb4be9b1aa645
Tuan Huynh <thuynh2001.dw@gmail.com>
2023-09-15 04:59:26 +1000

    dynamic mapping metrics (metricName) to the page

--- components/metricsSetting/metricsSetting.tsx
+++ components/metricsSetting/metricsSetting.tsx
@@ -2,13 +2,15 @@ import React from "react"
 import styles from "./metricsSetting.module.css"; // Import the CSS module
 import { FaEdit, FaRegTrashAlt } from 'react-icons/fa';
 import Link from 'next/link';
+import { Button } from "semantic-ui-react";
 
 interface MetricsSettingProps {
+    metricName: string,
 }
 
-const MetricsSetting: React.FC<MetricsSettingProps> = () => {
+const MetricsSetting: React.FC<MetricsSettingProps> = ({ metricName }) => {
     return (
-        <div className={styles.metricContainer}>Complexity
+        <div className={styles.metricContainer}>{metricName}
             <Link href="/">
                 <FaEdit size={18} style={{ color: "#50C878", marginLeft: "20px" }} />
                 <FaRegTrashAlt size={18} style={{ color: "#EE4B2B", marginLeft: "15px" }} />
@@ -36,5 +38,5 @@ const MetricsSetting: React.FC<MetricsSettingProps> = () => {
         </div >
     );
 }
-
+    
 export default MetricsSetting;
--- pages/config.tsx
+++ pages/config.tsx
@@ -3,12 +3,71 @@ import MetricsSetting from "../components/metricsSetting/metricsSetting"
 
 const ConfigurationPage = () => {
 
+    var projectData = {
+        projectid: '643d2f9487baeec2c1c0c2d1',
+        metrics: [
+            {
+                metricName: 'Complexity',
+                levels: [
+                    {
+                        levelLabel: "Low",
+                        levelOrder: 1
+                    }, {
+                        levelLabel: "Medium",
+                        levelOrder: 2
+                    }, {
+                        levelLabel: "High",
+                        levelOrder: 3
+                    }
+                ],
+                metricId: '64f6c424d4c684fa3223598d'
+            },
+            {
+                metricName: 'Teamwork',
+                levels: [
+                    {
+                        levelLabel: "Low",
+                        levelOrder: 1
+                    }, {
+                        levelLabel: "Medium",
+                        levelOrder: 2
+                    }, {
+                        levelLabel: "High",
+                        levelOrder: 3
+                    }
+                ],
+                metricId: ''
+            },
+            {
+                metricName: 'Difficulty',
+                levels: [
+                    {
+                        levelLabel: "Low",
+                        levelOrder: 1
+                    }, {
+                        levelLabel: "Medium",
+                        levelOrder: 2
+                    }, {
+                        levelLabel: "High",
+                        levelOrder: 3
+                    }
+                ],
+                metricId: ''
+            }
+        ]
+    }
+
     return (
         <div className="body-config">
             <h1>Emotimonitor Configuration</h1>
             <p>This page allow you to manage the content shown in the Trello PowerUp, including modifying, adding, or removing metrics and adjusting emoji for each level.</p>
             <h2>Manage Metrics</h2>
-            <MetricsSetting/>
+            {
+                // metric is the current element of the projectData.metrics array, index is the index of the current element in the projectData.metrics array
+                projectData.metrics.map((metric, index) => (
+                    <MetricsSetting metricName={metric.metricName}/>
+                ))
+            }
         </div>
     );
 
