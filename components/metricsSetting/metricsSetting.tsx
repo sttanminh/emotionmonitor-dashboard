d78e9b0196c6c9f7b4a47a3eed5df05bbaa4b228
Tuan Huynh <thuynh2001.dw@gmail.com>
2023-09-17 16:22:55 +1000

    added entering and exiting editing mode

--- components/metricsSetting/metricsSetting.tsx
+++ components/metricsSetting/metricsSetting.tsx
@@ -1,7 +1,8 @@
-import React from "react"
+import React, { useState } from "react"
 import styles from "./metricsSetting.module.css"; // Import the CSS module
 import Link from 'next/link';
 import { FaEdit, FaRegTrashAlt } from 'react-icons/fa';
+import { Button } from "semantic-ui-react";
 
 // represent an element in the level array. Consist of a label and the order
 interface MetricLevel {
@@ -22,26 +23,60 @@ interface MetricsSettingProps {
 }
 
 const MetricsSetting: React.FC<MetricsSettingProps> = ({ metric }) => {
-    return (
-        <div className={styles.metricContainer}><b>{metric.metricName}</b>
-            <Link href="/">
-                <FaEdit size={18} style={{ color: "#50C878", marginLeft: "20px" }} />
-                <FaRegTrashAlt size={18} style={{ color: "#EE4B2B", marginLeft: "15px" }} />
-            </Link>
+    const [isEditing, setIsEditing] = useState(false);
 
+    const editButtonClick = () => {
+        setIsEditing(true);
+    }
+
+    const cancelButtonClick = () => {
+        setIsEditing(false);
+    }
+
+    const saveButtonClick = () => {
+        setIsEditing(false);
+    }
+
+
+    return (
+        <div className={styles.metricContainer}>
+            {isEditing ? (
+                // if the user is in editing mode
+                <input type="text" placeholder={metric.metricName} onChange={(e) => { }} />
+            ) : (
+                // if the user is not in editing mode
+                <b>{metric.metricName}</b>
+            )}
+            <button onClick={editButtonClick} className={styles.button}>
+                <FaEdit size={18} style={{ color: "#50C878", marginLeft: "5px" }} />
+            </button>
+            <button className={styles.button}>
+                <FaRegTrashAlt size={18} style={{ color: "#EE4B2B" }} />
+            </button>
             <div className={styles.levelsContainer}>
                 {
-                    // for each element in the levels array we add a level div using map()
-                    metric.levels.map((level) => ( // level is the current element of the metric.levels array
-                        <div className={styles.level}>{level.levelLabel}
-                            <Link href="/">
-                                <FaEdit size={18} style={{ color: "#50C878", marginLeft: "20px" }} />
-                                <FaRegTrashAlt size={18} style={{ color: "#EE4B2B", marginLeft: "15px" }} />
-                            </Link>
+                    // for each element in the levels array we add a div using map()
+                    metric.levels.map((level, index) => (
+                        <div key={index} className={styles.level}>
+                            {isEditing ? (
+                                // if the user is in editing mode
+                                <input type="text" placeholder={level.levelLabel} onChange={(e) => { }} />
+                            ) : (
+                                // if the user is not in eiditing mode
+                                level.levelLabel
+                            )}
                         </div>
                     ))
                 }
             </div>
+            {
+                // add a save and cancel button is the user is in editing mode
+                isEditing &&
+                <div>
+                    <button onClick={cancelButtonClick}>Cancel</button>
+                    <button onClick={saveButtonClick}>Save</button>
+                </div>
+            }
         </div >
     );
 }
