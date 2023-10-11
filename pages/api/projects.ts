2ed030725b4b0d169f338492a7759ae7b5c9e6b3
Ellen Welstead-Cloud <ewel0002@student.monash.edu>
2023-10-12 00:20:03 +1100

    fix: always show active metrics on graph

--- pages/api/projects.ts
+++ pages/api/projects.ts
@@ -24,11 +24,11 @@ async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
     }
   } else if (req.method === "PUT") {
     try {
-      const { projectData } = req.body
-      await configureProject(projectData)
+      const { projectData } = req.body;
+      await configureProject(projectData);
       res.status(201).json({ message: "Project configured!" });
     } catch (error: any) {
-      res.status(500).json({ message: error.message })
+      res.status(500).json({ message: error.message });
     }
   }
 }
@@ -40,6 +40,10 @@ async function getProjects() {
       trelloCards: {
         select: { taskName: true, id: true },
       },
+      metrics: {
+        where: { active: true },
+        include: { levels: true },
+      },
     },
   });
   await prisma.$disconnect();
@@ -49,149 +53,148 @@ async function getProjects() {
 export async function getProject(projectId: string) {
   return await prisma.project.findFirst({
     where: {
-      id: projectId
+      id: projectId,
     },
     include: {
       metrics: {
         include: {
-          levels: true
-        }
-      }
-    }
-  })
+          levels: true,
+        },
+      },
+    },
+  });
 }
 
 export async function configureProject(projectData: ProjectProps) {
-  await configureMetricsAndLevels(projectData)
-  await updateProjectEmojisAndReference(projectData)
+  await configureMetricsAndLevels(projectData);
+  await updateProjectEmojisAndReference(projectData);
 }
 
 async function configureMetricsAndLevels(projectData: ProjectProps) {
-  var projectId = projectData.projectId
-  var newMetrics: any[] = []
-  var pastMetrics: any[] = []
-  var existingMetrics: any[] = []
+  var projectId = projectData.projectId;
+  var newMetrics: any[] = [];
+  var pastMetrics: any[] = [];
+  var existingMetrics: any[] = [];
 
   var allMetrics = await prisma.metric.findMany({
     where: {
-      projectId: projectId
-    }
-  })
-  var metricNameArray = allMetrics.map(metricObject => metricObject.name)
+      projectId: projectId,
+    },
+  });
+  var metricNameArray = allMetrics.map((metricObject) => metricObject.name);
 
   // Sort incoming metrics
-  projectData.metrics.forEach(metric => {
-    if (metric.metricId && metric.metricId != '') {
-      existingMetrics.push(metric)
+  projectData.metrics.forEach((metric) => {
+    if (metric.metricId && metric.metricId != "") {
+      existingMetrics.push(metric);
     } else if (metricNameArray.includes(metric.metricName)) {
-      pastMetrics.push(metric)
+      pastMetrics.push(metric);
     } else {
-      newMetrics.push(metric)
+      newMetrics.push(metric);
     }
-  })
+  });
 
   // Deactivate all metrics
   await prisma.metric.updateMany({
     where: {
-      projectId: projectId
+      projectId: projectId,
     },
     data: {
-      active: false
-    }
-  })
+      active: false,
+    },
+  });
   // Create new metrics
   if (newMetrics.length > 0) {
-    var newMetricData = newMetrics.map(metric => {
+    var newMetricData = newMetrics.map((metric) => {
       return {
         name: metric.metricName,
-        projectId: projectId
-      }
-    })
+        projectId: projectId,
+      };
+    });
     await prisma.$transaction([
       prisma.metric.deleteMany({
         where: {
           name: {
-            in: newMetricData.map(metric => metric.name)
-          }
-        }
+            in: newMetricData.map((metric) => metric.name),
+          },
+        },
       }),
       prisma.metric.createMany({
-        data: newMetricData
+        data: newMetricData,
       }),
-    ])
+    ]);
   }
-
 
   // Reinstate existing metrics
   for await (var metric of existingMetrics) {
     await prisma.metric.update({
       where: {
-        id: metric.metricId
+        id: metric.metricId,
       },
       data: {
         name: metric.metricName,
-        active: true
-      }
-    })
+        active: true,
+      },
+    });
   }
 
   // Update past metrics
-  var pastMetricNames = pastMetrics.map(metric => metric.metricName)
+  var pastMetricNames = pastMetrics.map((metric) => metric.metricName);
   await prisma.metric.updateMany({
     where: {
       name: {
-        in: pastMetricNames
-      }
+        in: pastMetricNames,
+      },
     },
     data: {
-      active: true
-    }
-  })
+      active: true,
+    },
+  });
 
   var allMetrics = await prisma.metric.findMany({
     where: {
-      projectId: projectId
-    }
-  })
-  var allMetricIds = allMetrics.map(metric => metric.id)
-  var activeMetrics = allMetrics.filter(metric => metric.active)
-  var activeMetricsDictionary: any = {}
-  activeMetrics.forEach(metric => {
-    activeMetricsDictionary[metric.name] = metric.id
-  })
+      projectId: projectId,
+    },
+  });
+  var allMetricIds = allMetrics.map((metric) => metric.id);
+  var activeMetrics = allMetrics.filter((metric) => metric.active);
+  var activeMetricsDictionary: any = {};
+  activeMetrics.forEach((metric) => {
+    activeMetricsDictionary[metric.name] = metric.id;
+  });
   // Delete existing levels linked to project
   await prisma.level.deleteMany({
     where: {
       metricId: {
-        in: allMetricIds
-      }
-    }
-  })
+        in: allMetricIds,
+      },
+    },
+  });
   // Add new levels
-  var levelData: any[] = []
-  projectData.metrics.forEach(metric => {
-    metric.levels.forEach(level => {
+  var levelData: any[] = [];
+  projectData.metrics.forEach((metric) => {
+    metric.levels.forEach((level) => {
       levelData.push({
         levelLabel: level.levelLabel,
         levelOrder: level.levelOrder,
-        metricId: activeMetricsDictionary[metric.metricName]
-      })
-    })
-  })
+        metricId: activeMetricsDictionary[metric.metricName],
+      });
+    });
+  });
   await prisma.level.createMany({
-    data: levelData
-  })
+    data: levelData,
+  });
 }
 
 async function updateProjectEmojisAndReference(projectData: ProjectProps) {
   return await prisma.project.updateMany({
     where: {
-      id: projectData.projectId
+      id: projectData.projectId,
     },
     data: {
       emojis: projectData.emojis,
-      referenceNumber: projectData.referenceNumber
-    }
+      referenceNumber: projectData.referenceNumber,
+    },
   });
 }
 
